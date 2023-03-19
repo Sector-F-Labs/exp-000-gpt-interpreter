ac03ca2a96aee587b0e023ee0509b5f8a7f9ee90
Divan visagie <me@dvisagie.com>
2023-03-19 18:02:23 +0100

    Improved logging

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,5 +1,35 @@
-export const logger = {
-  info: console.log,
-  error: console.error
+import * as fs from 'fs'
+import * as path from 'path'
+
+const logsDir = './logs'
+const logFilePath = path.join(logsDir, 'app.log')
+
+// Create logs directory if it doesn't exist
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir)
 }
+
+// Create a writable stream for the log file
+const logStream = fs.createWriteStream(logFilePath, { flags: 'a' })
+
+// Custom log and error functions that write to the log file and console
+const log = (message: string, ...args: any) => {
+  const timestamp = new Date().toISOString()
+  const p = JSON.stringify(args)
+  const logMessage = `${timestamp} - INFO: ${message}\n ${p}\n`
+  logStream.write(logMessage)
+}
+
+const error = (message: string, ...args: any) => {
+  const timestamp = new Date().toISOString()
+  const p = JSON.stringify(args)
+  const errorMessage = `${timestamp} - ERROR: ${message}\n ${p}\n`
+  logStream.write(errorMessage)
+}
+
+export const logger = {
+  info: log,
+  error: error
+}
+
 export type Logger = typeof logger
